259cd4d53643a5768bc0a21f1cad046e3caebc27
kenny <97374837+kankenny@users.noreply.github.com>
2025-09-16 23:11:04 +0000

    feat: add (tbd) instructions to agent

--- transcendent/k.py
+++ transcendent/k.py
@@ -4,27 +4,37 @@ import os
 from smolagents import CodeAgent, InferenceClientModel, DuckDuckGoSearchTool
 
 
-def load_agent_k():
+def load_agent():
     load_dotenv()
+
     token = os.environ.get("SMOLAGENTS_API_KEY")
+    instructions = os.environ.get("AGENT_INTSTRUCTIONS")
 
     model = InferenceClientModel(token=token)
-    agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=model, max_steps=2)
+    agent = CodeAgent(
+        tools=[DuckDuckGoSearchTool()],
+        model=model,
+        max_steps=2,
+        instructions=instructions,
+    )
+
+    print(dir(agent))
+    print(agent.system_prompt)
 
     return agent
 
 
-def ask_agent_k(agent, prompt):
+def ask_agent(agent, prompt):
     result = agent.run(prompt)
     print(result)
 
 
 if __name__ == "__main__":
-    agent = load_agent_k()
+    k = load_agent()
 
     try:
         while True:
             prompt = input("\nHello there! (Ctrl+C to exit): ")
-            ask_agent_k(agent, prompt)
+            ask_agent(k, prompt)
     except KeyboardInterrupt:
         print("\nExiting. Goodbye!")
